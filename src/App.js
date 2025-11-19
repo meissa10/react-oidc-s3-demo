2d9dad78dd4eb533844fc9a680475331dfad0e71
meissa10 <56763829+meissa10@users.noreply.github.com>
2025-11-19 23:42:30 +0000

    test login 9

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     // Check for the sample account
     if (email === 'user@example.com' && password === 'password123') {
       
-      setStatus('Login successful! Redirecting...');
+      setStatus('Login successful! ');
       
       
       // It tells the browser to navigate to the new page.
